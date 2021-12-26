fbf499eb77c6922a377da48c59b72291e7936bee
Andrew Burke <nomnomab1@gmail.com>
2021-12-26 11:51:35 -0500

    (feature): Dual-usage of assets and objects

--- Editor/RenameWindow.cs
+++ Editor/RenameWindow.cs
@@ -1,153 +1,164 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace Nomnom.RegexRename.Editor {
-	/// <summary>
-	/// Overrides F2 in the hierarchy to allow for regex renaming
-	/// </summary>
-	internal sealed class RenameWindow: EditorWindow {
-		private Object[] _objects;
-		private string _regexInput;
-		private string _lastRegex;
-		private string _replacement;
-		private Regex _regexObject;
-		private Vector2 _scrollValue;
-		private bool _inProjectMode;
-		private bool _hasFocused = false;
-		
-		[InitializeOnLoadMethod]
-		private static void Load() {
-			EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
-			EditorApplication.projectWindowItemOnGUI += OnProjectGUI;
-		}
+  /// <summary>
+  /// Overrides F2 in the hierarchy to allow for regex renaming
+  /// </summary>
+  internal sealed class RenameWindow : EditorWindow {
+    private Object[] _objects;
+    private string _regexInput;
+    private string _lastRegex;
+    private string _replacement;
+    private Regex _regexObject;
+    private Vector2 _scrollValue;
+    private bool _hasProjectAsset;
+    private bool _hasFocused = false;
 
-		private static void OnProjectGUI(string guid, Rect selectionRect) {
-			HandleEvent();
-		}
-		
-		private static void OnHierarchyGUI(int instanceID, Rect rect) {
-			HandleEvent();
-		}
+    [InitializeOnLoadMethod]
+    private static void Load() {
+      EditorApplication.hierarchyWindowItemOnGUI += OnHierarchyGUI;
+      EditorApplication.projectWindowItemOnGUI += OnProjectGUI;
+    }
 
-		private static void HandleEvent() {
-			Event e = Event.current;
-			Object[] objects = Selection.objects;
+    private static void OnProjectGUI(string guid, Rect selectionRect) {
+      HandleEvent();
+    }
 
-			if (objects.Length <= 1 || e.type != EventType.KeyDown || e.keyCode != KeyCode.F2) {
-				return;
-			}
+    private static void OnHierarchyGUI(int instanceID, Rect rect) {
+      HandleEvent();
+    }
 
-			bool hasAsset = false;
-			bool hasScene = false;
-			foreach (Object obj in objects) {
-				if (AssetDatabase.IsMainAsset(obj)) {
-					hasAsset = true;
-				} else {
-					hasScene = true;
-				}
-			}
+    private static void HandleEvent() {
+      Event e = Event.current;
+      Object[] objects = Selection.objects;
 
+      if (objects.Length <= 1 || e.type != EventType.KeyDown || e.keyCode != KeyCode.F2) {
+        return;
+      }
+
+      bool hasAsset = false;
+      foreach (Object obj in objects) {
+        if (AssetDatabase.IsMainAsset(obj)) {
+          hasAsset = true;
+        } else {
+        }
+      }
+
+#if NOM_RENAME_NO_MIX
 			if (hasAsset && hasScene) {
 				return;
 			}
+#endif
 
-			Open(objects);
-				
-			e.Use();
-		}
+      Open(objects, hasAsset);
 
-		private static void Open(Object[] objects) {
-			RenameWindow window = GetWindow<RenameWindow>("Rename");
-			window.OnDestroy();
-			window._objects = objects;
-			window.Show();
+      e.Use();
+    }
 
-			window._inProjectMode = AssetDatabase.IsMainAsset(objects[0]);
-		}
+    private static void Open(Object[] objects, bool hasProjectAssets) {
+      RenameWindow window = GetWindow<RenameWindow>("Rename");
+      window.OnDestroy();
+      window._objects = objects;
+      window.Show();
 
-		private void OnDestroy() {
-			_objects = null;
-			_regexInput = string.Empty;
-			_lastRegex = string.Empty;
-			_regexObject = null;
-			_replacement = string.Empty;
-			_scrollValue = Vector2.zero;
-		}
+      window._hasProjectAsset = hasProjectAssets;
+    }
 
-		static bool IsKeyDown(KeyCode keyCode) =>
-			Event.current != null && Event.current.isKey && Event.current.keyCode == keyCode;
+    private void OnDestroy() {
+      _objects = null;
+      _regexInput = string.Empty;
+      _lastRegex = string.Empty;
+      _regexObject = null;
+      _replacement = string.Empty;
+      _scrollValue = Vector2.zero;
+    }
 
-		private void OnGUI() {
-			const string ReplacementName = nameof(ReplacementName);
+    private static bool IsKeyDown(KeyCode keyCode) {
+      return Event.current != null && Event.current.isKey && Event.current.keyCode == keyCode;
+    }
 
-			_regexInput = EditorGUILayout.TextField("Pattern (optional)", _regexInput);
-			GUI.SetNextControlName(ReplacementName);
-			_replacement = EditorGUILayout.TextField("Replacement", _replacement);
+    private void OnGUI() {
+      const string ReplacementName = nameof(ReplacementName);
 
-			string regex = string.IsNullOrEmpty(_regexInput)
-				? ".+"
-				: _regexInput;
+      _regexInput = EditorGUILayout.TextField("Pattern (optional)", _regexInput);
+      GUI.SetNextControlName(ReplacementName);
+      _replacement = EditorGUILayout.TextField("Replacement", _replacement);
 
-			if (!_hasFocused) {
-				EditorGUI.FocusTextInControl(ReplacementName);
-				_hasFocused = true;
-			}
+      string regex = string.IsNullOrEmpty(_regexInput)
+        ? ".+"
+        : _regexInput;
 
-			if (regex != _lastRegex) {
-				_lastRegex = regex;
-				_regexObject = string.IsNullOrEmpty(regex) ? null : new Regex(regex);
-			}
+      if (!_hasFocused) {
+        EditorGUI.FocusTextInControl(ReplacementName);
+        _hasFocused = true;
+      }
 
-			bool validReplacement = !string.IsNullOrEmpty(_replacement);
+      if (regex != _lastRegex) {
+        _lastRegex = regex;
+        _regexObject = string.IsNullOrEmpty(regex) ? null : new Regex(regex);
+      }
 
-			if (_inProjectMode) {
-				EditorGUILayout.HelpBox("Changes done to assets cannot be undone", MessageType.Warning);
-			}
+      bool validReplacement = !string.IsNullOrEmpty(_replacement);
 
-			GUI.enabled = _regexObject != null && validReplacement;
-			if (GUILayout.Button("Replace") || (GUI.enabled && IsKeyDown(KeyCode.Return))) {
-				foreach (Object obj in _objects) {
-					string newName = _regexObject.Replace(obj.name, _replacement);
-					
-					Undo.RecordObject(obj, "Changed name");
-					if (_inProjectMode) {
-						AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(obj), newName);
-					} else {
-						obj.name = newName;
-						PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
-					}
-				}
+      if (_hasProjectAsset) {
+        EditorGUILayout.HelpBox("Changes done to assets cannot be undone", MessageType.Warning);
+      }
 
-				if (_inProjectMode) {
-					AssetDatabase.SaveAssets();
-					AssetDatabase.Refresh();
-					// Force repaint in case rename was performed by keyboard.
-					Repaint();
-				} else {
-					// Only close the window outside of "project mode". (Can't
-					// do it in project mode because we need to show the undo
-					// button.
-					Close();
-					return;
-				}
-			}
-			GUI.enabled = true;
-			
-			EditorGUILayout.PrefixLabel("Preview", EditorStyles.boldLabel);
-			
-			GUI.backgroundColor = Color.white * 0.2f;
-			_scrollValue = EditorGUILayout.BeginScrollView(_scrollValue, "Wizard Box");
-			foreach (Object obj in _objects) {
-				EditorGUILayout.BeginHorizontal();
-				{
-					string name = obj.name;
-					EditorGUILayout.LabelField(name);
-					EditorGUILayout.LabelField(_regexObject == null || !validReplacement ? name : _regexObject.Replace(name, _replacement));
-				}
-				EditorGUILayout.EndHorizontal();
-			}
-			EditorGUILayout.EndScrollView();
-		}
-	}
+      GUI.enabled = _regexObject != null && validReplacement;
+      if (GUILayout.Button("Replace") || (GUI.enabled && IsKeyDown(KeyCode.Return))) {
+        IEnumerable<Object> assets = _objects.Where(obj => AssetDatabase.IsMainAsset(obj));
+        IEnumerable<Object> sceneObjects = _objects.Where(obj => !AssetDatabase.IsMainAsset(obj));
+
+        foreach (Object obj in assets) {
+          string newName = _regexObject.Replace(obj.name, _replacement);
+          string msg = AssetDatabase.RenameAsset(AssetDatabase.GetAssetPath(obj), newName);
+          if (!string.IsNullOrEmpty(msg)) {
+            Debug.LogError(msg);
+          }
+        }
+
+        foreach (Object obj in sceneObjects) {
+          string newName = _regexObject.Replace(obj.name, _replacement);
+          Undo.RecordObject(obj, "Changed name");
+          obj.name = newName;
+          PrefabUtility.RecordPrefabInstancePropertyModifications(obj);
+        }
+
+        if (sceneObjects.Count() > 0) {
+          EditorSceneManager.SaveScene(EditorSceneManager.GetActiveScene());
+        }
+
+        if (_hasProjectAsset) {
+          AssetDatabase.SaveAssets();
+          AssetDatabase.Refresh();
+          // Force repaint in case rename was performed by keyboard.
+          Repaint();
+        }
+
+        Close();
+        return;
+      }
+      GUI.enabled = true;
+
+      EditorGUILayout.PrefixLabel("Preview", EditorStyles.boldLabel);
+
+      GUI.backgroundColor = Color.white * 0.2f;
+      _scrollValue = EditorGUILayout.BeginScrollView(_scrollValue, "Wizard Box");
+      foreach (Object obj in _objects) {
+        _ = EditorGUILayout.BeginHorizontal();
+        {
+          string name = obj.name;
+          EditorGUILayout.LabelField(name);
+          EditorGUILayout.LabelField(_regexObject == null || !validReplacement ? name : _regexObject.Replace(name, _replacement));
+        }
+        EditorGUILayout.EndHorizontal();
+      }
+      EditorGUILayout.EndScrollView();
+    }
+  }
 }
--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Regex Rename
 Select multiple scene objects or assets, and press `F2` to open the new regex rename window.
 
-![Preview](./Assets~/preview.gif)
-
 ## Installation
 - OpenUPM
   - `openupm add com.nomnom.regex-rename`
 - Package Manager
   - Add through git url `https://github.com/nomnomab/Regex-Rename.git`
+
+![Preview](./Assets~/preview.gif)
